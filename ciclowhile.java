43c3f59e861a245032cc67b43965ede99b5ae496
Jesus Miguel <102165800+Jesus2421@users.noreply.github.com>
2023-12-01 19:09:13 +0100

    Create ciclowhile.java

---
+++ ciclowhile.java
@@ -0,0 +1,10 @@
+class ciclowhile {
+	public static void main (String args[]){
+		int i = 1;
+		while (i<=100){
+			if (i % 2 == 0) 
+		     System.out.println("el numero:"+ i +" es par");
+		     i++;
+		}
+	}
+}
