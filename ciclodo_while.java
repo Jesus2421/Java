7718473d6db91252f3f732b344acd62da4a973bf
Jesus Miguel <102165800+Jesus2421@users.noreply.github.com>
2023-12-01 19:09:03 +0100

    Create ciclodo_while.java

---
+++ ciclodo_while.java
@@ -0,0 +1,10 @@
+class ciclodo_while {
+	public static void main (String args[]){
+		int i = 1;
+		do {
+			if (i % 2 != 0) 
+		     System.out.println("el numero:"+ i +" es impar");
+		     i++;
+		} while (i<=100);
+	}
+}
