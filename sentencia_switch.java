8a08175bc4fb99272ea462c994d5db083b45ff34
Jesus Miguel <102165800+Jesus2421@users.noreply.github.com>
2023-12-01 18:54:02 +0100

    Create sentencia_switch.java

---
+++ sentencia_switch.java
@@ -0,0 +1,12 @@
+class sentencia_switch {
+	 public static void main (String args[]){
+		 int i = 2;
+		 switch (i) {
+			 case 1: System.out.println("i tiene valor 1");
+			  break;
+			 case 2: System.out.println("i tiene valor 2");
+			  break;
+			 default: System.out.println("su opcion no existe");
+		 }
+	 }
+}
