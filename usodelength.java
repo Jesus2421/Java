881b462f6135ae46e84efcc0ae983e10a7fee2e7
Jesus Miguel <102165800+Jesus2421@users.noreply.github.com>
2023-12-01 18:53:08 +0100

    Create usodelength.java

---
+++ usodelength.java
@@ -0,0 +1,8 @@
+class usodelength {
+	public static void main (String args[]){
+		System.out.println("el metodo principal recibe:");
+		for (int i=0; i<args.length; i++){
+			System.out.println(args[i]);
+		}
+	}
+}
